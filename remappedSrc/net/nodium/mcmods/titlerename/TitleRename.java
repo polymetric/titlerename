a21fa2dbe10edd15db10407a2cf62630464559ca
Polymetric <polymetricofficial@gmail.com>
2020-08-25 17:46:09 -0400

    configure template

---
+++ remappedSrc/net/nodium/mcmods/titlerename/TitleRename.java
@@ -0,0 +1,9 @@
+package net.nodium.mcmods.titlerename;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TitleRename implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
